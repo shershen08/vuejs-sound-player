d8fca55be0243270cc02f43cdb7d35195b5fb37b
Mikhail Kuznetcov <michail.kuznetsov@gmail.com>
2017-11-22 10:15:28 +0100

    Update vueaudio.plugin.js
    put into one declaration

--- src/vueaudio.plugin.js
+++ src/vueaudio.plugin.js
@@ -4,9 +4,8 @@
 
   const generateUUID = () => {
       /* eslint-disable */
-    let v;
     return 'xxxxxxxx-xxxx-4xxx'.replace(/[xy]/g, function (c) {
-      var r = Math.random() * 16 | 0; v = c === 'x' ? r : (r & 0x3 | 0x8)
+      var v, r = Math.random() * 16 | 0; v = c === 'x' ? r : (r & 0x3 | 0x8)
       return v.toString(16)
     })
      /* eslint-enable */
