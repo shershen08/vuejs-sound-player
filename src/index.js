6678a3bd5a5c5da6d3f72cb5d7ca91f630375408
lanzhiheng <lanzhihengrj@gmail.com>
2017-11-23 17:07:15 +0800

    Re #6 add webpack scaffolding for development

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue/dist/vue.esm'
+import VueAudio from './VueAudio.vue'
+
+const Audio = new Vue({
+  el: '#audio',
+  components: {
+    VueAudio
+  }
+})
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+    publicPath: '/'
+  },
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            scss: 'vue-style-loader!css-loader!sass-loader', // <style lang="scss">
+            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax' // <style lang="sass">
+          }
+        }
+      }
+    ]
+  }
+}
